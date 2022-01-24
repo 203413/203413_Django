ce53b199ae2d3859c553586ca93d3d3a38232084
203413 <77992655+203413@users.noreply.github.com>
2022-01-23 19:56:47 -0600

    Created second component (login)

--- primerComponente/urls.py
+++ primerComponente/urls.py
@@ -1,10 +1,9 @@
-from django import views
-from django.urls import URLPattern
-from django.urls import path,re_path
+from django.urls import path, re_path
 from django.conf.urls import include
+
 
 from primerComponente.views import PrimerTablaList
 
-urlpatterns=[
-    re_path(r'^primer_componente/$',PrimerTablaList.as_view())
+urlpatterns = [
+    re_path(r'^lista/$', PrimerTablaList.as_view()),    
 ]
