5c7d1040bf6799e318d0eba9e50dafa92839acd1
203413 <77992655+203413@users.noreply.github.com>
2022-01-31 13:17:09 -0600

    Delete to primerComponente.views added

--- primerComponente/views.py
+++ primerComponente/views.py
@@ -39,7 +39,7 @@ class PrimerTablaDetail(APIView):
         if idResponse != 0:
             idResponse = PrimerTablaSerializer(idResponse)
             return Response(idResponse.data, status = status.HTTP_200_OK)
-        return Response("No hay datos deja checo como te arreglo mi pana",status = status.HTTP_400_BAD_REQUEST)
+        return Response("No hay datos",status = status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk, format=None):
         idResponse = self.get_object(pk)
@@ -49,3 +49,11 @@ class PrimerTablaDetail(APIView):
             datas = serializer.data
             return Response(datas,status =status.HTTP_201_CREATED)
         return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        objetive = self.get_object(pk)
+        if objetive!="No existe":
+            objetive.delete()
+            return Response("Eliminao",  status = status.HTTP_200_OK)
+        else:
+            return Response("Dato no econtrado", status = status.HTTP_400_BAD_REQUEST)
